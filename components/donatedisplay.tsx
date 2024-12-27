33931510f82df1bbcf3263143e19c4fb2a536d51
PhyoMinKhant-Xem <phyominkhant.xem@gmail.com>
2024-12-27 07:41:09 +0630

    Update UI

--- components/donatedisplay.tsx
+++ components/donatedisplay.tsx
@@ -9,9 +9,9 @@ export default function DonateDisplay() {
   ];
 
   return (
-    <div className="w-full font-sans mt-20">
+    <div className="w-full font-sans md:mt-16 lg:mt-20 mt-14 ">
       {/* First Row: Centered Topic */}
-      <div className="flex justify-center items-center py-3 bg-gray-200 border-b-2 border-gray-300">
+      <div className="flex justify-center items-center py-3 bg-main text-white">
         <h2 className="text-lg font-semibold">ရေဘေးအလှူ</h2>
       </div>
 
@@ -21,10 +21,10 @@ export default function DonateDisplay() {
           {donations.concat(donations).map((donation, index) => (
             <div
               key={index}
-              className="flex px-4 py-2 bg-gray-100 rounded-lg shadow-md min-w-[200px] text-center"
+              className="flex items-center px-4 py-2 bg-gray-100 rounded-lg shadow-md text-center text-nowrap"
             >
               <h4 className="font-semibold">{donation.name}</h4>
-              <p className="font-bold">{donation.batch}</p>
+              <p className="font-bold mx-3">{donation.batch}</p>
               <p className="text-green-600">{donation.amount}</p>
             </div>
           ))}
