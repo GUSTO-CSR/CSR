3f05a553301ca0892e61e98bdf61921fe04fb809
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-30 16:29:15 +0630

    feat: donation delete function

--- app/csrsadmin/apis/donation/admin_donation.ts
+++ app/csrsadmin/apis/donation/admin_donation.ts
@@ -75,8 +75,8 @@ async function updateDonation(
 
       // Update the donation in the array
       event.Donations[donationIndex] = {
-        ...(event.Donations[donationIndex].toObject() as IDonation), // Retain existing fields
-        ...updatedDonation, // Overwrite with updated fields
+        ...(event.Donations[donationIndex].toObject() as IDonation),
+        ...updatedDonation,
       } as IDonation;
     }
 
@@ -91,6 +91,54 @@ async function updateDonation(
     console.error("Failed to update donation: ", error);
     response.error = true;
     response.message = "Error Updating Donation!";
+    return JSON.stringify(response);
+  }
+}
+
+async function deleteDonation(
+  eventId: number,
+  donationId: number
+): Promise<string> {
+  await connectMongo();
+  const response: CustomResponse<null> = {
+    status: false,
+    message: "No Error Message Provided!",
+  };
+
+  try {
+    const event: IEvent | null = await EventModel.findById(eventId);
+    if (!event) {
+      response.data = null;
+      response.message = "Event Not Found!";
+      return JSON.stringify(response);
+    }
+
+    // Find the donation by ID and remove it
+    if (event.Donations) {
+      const donationIndex = event.Donations.findIndex(
+        (d) => d._id === donationId
+      );
+      if (donationIndex === -1) {
+        response.data = null;
+        response.message = "Donation Not Found!";
+        return JSON.stringify(response);
+      }
+
+      // Remove the donation from the array
+      event.Donations.splice(donationIndex, 1);
+    }
+
+    // Save the updated event
+    await event.save();
+
+    response.status = true;
+    response.message = "Donation deleted successfully!";
+    response.data = null;
+    return JSON.stringify(response);
+  } catch (error) {
+    console.error("Failed to delete donation: ", error);
+    response.error = true;
+    response.message = "Error Deleting Donation!";
     return JSON.stringify(response);
   }
 }
